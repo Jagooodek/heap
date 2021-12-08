6df92dab53cce5c52ebc51c01062d6d9e03f7924
Paweł Wojciechowski <jagodek@pop-os.localdomain>
2021-12-08 16:05:57 +0100

    delete executable files

--- 1.c
+++ 1.c
@@ -14,11 +14,24 @@ int Cmp(int a, int b) {
 
 int main() {
     setCmp(&Cmp);
-    int n;
-    printf(u8"Podaj długość listy\n");
-    scanf("%d", n);
+    int n, index;
+    printf(u8"Podaj długość listy.\n");
+    scanf("%d", &n);
     
-    struct dataStruct  *data = malloc
+    struct dataStruct  *data = malloc(sizeof(struct dataStruct) * n);
+
+    printf(u8"Podaj listę.\n");
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s %s %d ", data[i].firstName, data[i].lastName, &data[i].age);
+        push(i, data[i].age);
+    }
+
+    for(int i = 0; i < n; i++) {
+        index = topKey();
+        pop();
+        printf("%s %s %d\n", data[index].firstName, data[index].lastName, data[index].age);
+    }
     
     return 0;
 }
